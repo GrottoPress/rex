a11efa9e9c47a4678638994001add5758eed35eb
akadusei <attakusiadusei@gmail.com>
2022-01-04 07:03:07 +0000

    Allow `Rex.l` helper to accept named arguments

--- src/rex.cr
+++ src/rex.cr
@@ -12,7 +12,7 @@ module Rex
     Rex.settings.adapter.translate(text, *args, **named_args)
   end
 
-  def self.l(value, *args)
-    Rex.settings.adapter.localize(value, *args)
+  def self.l(value, *args, **named_args)
+    Rex.settings.adapter.localize(value, *args, **named_args)
   end
 end
